f16b224c5111f0b6fd401e3fd42fa4fd1f3468c1
charlesjamb <charles.jambon@gmail.com>
2016-11-16 20:04:44 -0500

    testing class es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,20 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const del = require('del');
-const exec = require('child_process').exec;
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import del from 'del';
+import { exec } from 'child_process';
 
 const paths = {
 	allSrcJs: 'src/**/**/*.js',
 	libDir: 'lib',
 };
 
-gulp.task('clean', () => {
-	return del(paths.libDir);
-});
+gulp.task('clean', () => del(paths.libDir));
 
-gulp.task('build', ['clean'], () => {
-	return gulp.src(paths.allSrcJs)
+gulp.task('build', ['clean'], () =>
+	gulp.src(paths.allSrcJs)
 		.pipe(babel())
-		.pipe(gulp.dest(paths.libDir));
-});
+		.pipe(gulp.dest(paths.libDir))
+);
 
 gulp.task('main', ['build'], (callback) => {
 	exec(`node ${paths.libDir}`, (error, stdout) => {
