4b04686b605e8d4124058889d96a47319452f9bd
charlesjamb <charles.jambon@gmail.com>
2016-11-16 20:31:59 -0500

    webpack installed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,25 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import { exec } from 'child_process';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
-	allSrcJs: 'src/**/**/*.js',
-	libDir: 'lib',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
+  gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
+  libDir: 'lib',
+  distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(.map)',
 };
 
-gulp.task('clean', () => del(paths.libDir));
+gulp.task('clean', () => del([
+	paths.libDir,
+	paths.clientBundle,
+]));
 
 gulp.task('build', ['clean'], () =>
 	gulp.src(paths.allSrcJs)
@@ -16,12 +27,11 @@ gulp.task('build', ['clean'], () =>
 		.pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['build'], (callback) => {
-	exec(`node ${paths.libDir}`, (error, stdout) => {
-		console.log(stdout);
-		return callback(error);
-	});
-});
+gulp.task('main', ['clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+);
 
 gulp.task('watch', () => {
 	gulp.watch(paths.allSrcJs, ['main']);
